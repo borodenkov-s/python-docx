817d491f5ff0957f41b0ad04741cd35d0b0ee297
amorris <web@myemptybucket.com>
2013-01-29 12:26:09 +0000

    Structural update

--- setup.py
+++ setup.py
@@ -2,24 +2,57 @@
 
 from distutils.core import setup
 from glob import glob
+from distutils.command.build_py import build_py
 
 # Make data go into site-packages (http://tinyurl.com/site-pkg)
 from distutils.command.install import INSTALL_SCHEMES
 for scheme in INSTALL_SCHEMES.values():
     scheme['data'] = scheme['purelib']
+    
+long_description = """
+The docx module creates, reads and writes Microsoft Office Word 2007 docx files.  The docx module has the following features:
+
+Making documents:
+
+- Paragraphs
+- Bullets
+- Numbered lists
+- Document properties (author, company, etc)
+- Multiple levels of headings
+- Tables
+- Section and page breaks
+- Images
+
+
+Editing documents:
+
+- Search and replace
+- Extract plain text of document
+- Add and delete items anywhere within the document
+- Change document properties
+- Run xpath queries against particular locations in the document - useful for retrieving data from user-completed templates.
+
+"""
 
 setup(name='docx',
-      version='0.0.2',
+      version='0.0.3',
       requires=['lxml'],
       description='The docx module creates, reads and writes Microsoft Office Word 2007 docx files',
+      long_description=long_description,
       author='Mike MacCana',
       author_email='python.docx@librelist.com',
+      license="MIT License",
       url='http://github.com/mikemaccana/python-docx',
-      py_modules=['docx'],
-      data_files=[
-          ('docx-template/_rels', glob('template/_rels/.*')),
-          ('docx-template/docProps', glob('template/docProps/*.*')),
-          ('docx-template/word', glob('template/word/*.xml')),
-          ('docx-template/word/theme', glob('template/word/theme/*.*')),
+      download_url='http://github.com/mikemaccana/python-docx/tarball/master',
+      classifiers = [
+          "Development Status :: 3 - Alpha",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Programming Language :: Python :: 2",
+          "Programming Language :: Python :: 3",
+          "Operating System :: OS Independent",
+          "Topic :: Software Development :: Libraries :: Python Modules",
           ],
+      packages=["docx"],
+      package_data={'docx': ['docx-template/_rels/.*', 'docx-template/docProps/*.*', 'docx-template/word/*.xml', 'docx-template/word/theme/*.*']},
       )
